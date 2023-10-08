da15ec55b43ccde06e6b02be719d0030800e4449
akshaygp18 <akshaygp41@gmail.com>
2023-10-08 19:52:48 +0530

    prediction step added

--- src/main.py
+++ src/main.py
@@ -24,7 +24,10 @@ def main():
     with mlflow.start_run() as run:
         mlflow.run(".", "get_data", use_conda=False)
         mlflow.run(".", "base_model_creation", use_conda=False)
-        mlflow.run(".", "training", use_conda=False)    
+        mlflow.run(".", "training", use_conda=False) 
+        mlflow.run(".", "prediction", use_conda=False)
+
+
     
 
 
